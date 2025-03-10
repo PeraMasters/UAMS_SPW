049e67eace907ccab46bebc095f65c6ebac31e07
Maheshi <maheshirj4@gmail.com>
2025-03-10 22:43:48 +0530

    updated the routes of app.js file

--- uams/src/App.js
+++ uams/src/App.js
@@ -1,7 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Login from "./components/Login";
+import ExamDashboard from "./components/exam/ExamDashboard";
+import AcademicCordinatorDashboard from "./components/academic-cordinator/AcademicCordinatorDashboard";
+import TimetableDashboard from "./components/timetable/TimetableDashboard";
+import StudentControlDashboard from "./components/student-control/StudentControlDashboard";
+import StudentDashboard from "./components/student/StudentDashboard";
+import "./components/Dashboard.css";
+
+// Protected Route Component
+const ProtectedRoute = ({ children }) => {
+  // In a real app, you would check if the user is authenticated
+  // For now, we'll just render the children
+  return children;
+};
 
 function App() {
   return (
@@ -10,6 +23,51 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        
+        {/* Protected Routes for different roles */}
+        <Route 
+          path="/exam-dashboard" 
+          element={
+            <ProtectedRoute>
+              <ExamDashboard />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/academic-cordinator-dashboard" 
+          element={
+            <ProtectedRoute>
+              <AcademicCordinatorDashboard />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/timetable-dashboard" 
+          element={
+            <ProtectedRoute>
+              <TimetableDashboard />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/student-control-dashboard" 
+          element={
+            <ProtectedRoute>
+              <StudentControlDashboard />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/student-dashboard" 
+          element={
+            <ProtectedRoute>
+              <StudentDashboard />
+            </ProtectedRoute>
+          } 
+        />
+        
+        {/* Fallback route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
